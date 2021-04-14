f365b5481dd42a1ff7b4bacefebf534a101ef8ef
张月聪 <zhangyc@ncmps.com>
2021-04-14 15:08:47 +0800

    new remove slice string

--- slice/slice.go
+++ slice/slice.go
@@ -80,6 +80,23 @@ func RemoveSlice(slc []int64)[]int64{
 	return result
 }
 
+func RemoveSliceString(slc []string)[]string{
+	result := []string{}  // 存放结果
+	for i := range slc{
+		flag := true
+		for j := range result{
+			if slc[i] == result[j] {
+				flag = false  // 存在重复元素，标识为false
+				break
+			}
+		}
+		if flag {  // 标识为false，不添加进结果
+			result = append(result, slc[i])
+		}
+	}
+	return result
+}
+
 func InSlice(val string,slc []string) bool{
 	for _,v := range slc{
 		if v == val{
